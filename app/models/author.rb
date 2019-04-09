04cf364030616d5e1a269b0f6282841d875e8f69
danfyfe <fyfedaniel@gmail.com>
2019-04-09 10:41:29 -0400

    Did my best

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,52 @@
 class Author
-  attr_accessor :name
 
+  attr_reader :name
+
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  ## instance methods
+  def add_article(magazine,title,content)
+    Article.new(self,magazine,title,content)
+  end
+
+  def articles
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def magazines
+    self.articles.map do |article|
+      article.magazine
+    end.uniq
+  end
+
+  def show_specialties
+    self.articles.map do |article|
+      article.magazine.category
+    end.uniq
+  end
+
+  ## class methods
+  def self.all
+    @@all
+  end
+
+  def self.most_verbose
+    Article.all.max_by do |article|
+      article.content.length
+    end.author
+  end
+
+  def self.most_active
+    self.all.max_by do |author|
+      author.articles.length
+    end
+  end
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,53 @@
 class Magazine
+
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self
   end
 
+  ## instance methods
+  ## helper method
+  def articles
+    Article.all.select do |article|
+      article.magazine == self
+    end
+  end
+
+  ## instance methods(cont)
+  def article_titles
+    self.articles.map do |article|
+      article.title
+    end
+  end
+
+  def contributors
+    self.articles.map do |article|
+      article.author
+    end
+  end
+
+  def word_count
+    word_count = 0
+    self.articles.each do |article|
+      word_count += article.content.split.length
+    end
+    word_count
+  end
+
+  ## class methods
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(mag_name)
+    self.all.find do |magazine|
+      magazine.name == mag_name
+    end
+  end
 
 end
