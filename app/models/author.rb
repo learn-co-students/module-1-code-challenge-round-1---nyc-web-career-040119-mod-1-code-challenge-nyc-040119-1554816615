2ccce83a4e8a1605c02eb3c84b688b439489d317
Edgar Ong <edgar@minus.com>
2019-04-09 10:37:15 -0400

    here we go

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,62 @@
+require 'pry'
+
 class Author
-  attr_accessor :name
+  attr_reader :name
 
-
+  @@all = []
   def initialize(name)
     @name = name
-  
+
+    @@all << self
   end
 
+  # INSTANCE METHODS
+  def add_article(magazine_object, title, content)
+    Article.new(self, magazine_object, title, content)
+  end
+
+  # HELPER
+  def article_author
+    Article.all.select do |article|
+      article.author == self
+    end
+  end
+
+  def articles
+    self.article_author
+  end
+
+  def magazines
+    article_author.map do |article|
+      article.magazine
+    end.uniq
+  end
+
+  def show_specialties
+    self.magazines.map do |magazine|
+      magazine.category
+    end
+  end
+
+  # CLASS METHODS
+  def self.all
+    @@all
+  end
+
+  def self.most_verbose
+    Article.all.max_by do |article|
+      article.content.length
+    end.author
+  end
+
+  def self.most_active
+    hash = Hash.new(0)
+    Article.all.map do |article|
+      hash[article.author] += 1
+    end
+    hash.sort_by do |author, count|
+      count
+    end.reverse.to_h.first[0]
+  end
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,50 @@
 class Magazine
   attr_accessor :name, :category
 
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
 
+    @@all << self
   end
 
+  # INSTANCE METHODS
+
+  # HELPER
+  def article_magazine
+    Article.all.select do |article|
+      article.magazine == self
+    end
+  end
+
+  def article_titles
+    self.article_magazine.map do |article|
+      article.title
+    end
+  end
+
+  def contributors
+    self.article_magazine.map do |article|
+      article.author
+    end
+  end
+
+  def word_count
+    self.article_magazine.map do |article|
+      article.content.length
+    end.inject(:+)
+  end
+
+  # CLASS METHODS
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(magazine_name_str)
+    self.all.find do |magazine|
+      magazine.name == magazine_name_str
+    end
+  end
 
 end
