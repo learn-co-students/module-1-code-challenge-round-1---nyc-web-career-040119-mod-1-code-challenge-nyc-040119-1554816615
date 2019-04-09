121a2e4563ffbe325439bad3e70b7e7a3a1afa6b
Daviicode <d.ydzdavid@yahoo.com>
2019-04-09 10:43:05 -0400

    did my best

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,44 @@
 class Magazine
   attr_accessor :name, :category
 
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
 
+
+    @all << self
   end
 
+#class methods
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.detect do |magazine|  #return the first true in this block
+      magazine.name == name
+    end
+  end
+
+
+  #instant methods
+
+  def article_titles
+    Article.all.select do |artile_title|
+      artile_title.magazine == self
+    end
+  end
+
+  def contributors
+    Author.all.select do |name|
+      name.author == self
+    end
+  end
+
+  def word_count
+    Article.all.select do |words|
+      words.content == self
+    end.length.to_i
 
 end
--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,20 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+  magazine1 = Magazine.new("NY time", "news")
+  magazine2 = Magazine.new("Boston time", "news")
+  magazine3 = Magazine.new("some time", "random")
+  magazine4 = Magazine.new("Autoban", "cars")
+
+  article1 = Article.new(david, magazine1, "some_news", "random_news")
+  article2 = Article.new(jeff, magazine2, "daily_news", "random news with the most content")
+  article2 = Article.new(david, magazine4, "caymans buyer guide", "cars")
+  article2 = Article.new(will, magazine3, "just read it", "random stuff")
+
+  david = Author.new("david.z")
+  jeff = Author.new("jeff.b")
+  will = Author.new("will.s")
+
 
 
 
