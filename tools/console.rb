93193a8be91a3a9f02bfeed7628e1b76cd8035ba
github username <github email address>
2019-04-09 10:43:36 -0400

    Welp

--- tools/console.rb
+++ tools/console.rb
@@ -7,6 +7,17 @@ end
 
 ###  WRITE YOUR TEST CODE HERE ###
 
+#AUTHORS
+jk_rowling = Author.new("J.K. Rowling")
+nick_sparks = Author.new("Nicholas Sparks")
+
+#MAGAZINES
+maxim = Magazine.new("Maxim", "Men Stuff")
+sports_illustrated = Magazine.new("Sports Illustrated", "Sports")
+
+#ARTICLES (JOIN TABLE)
+diet = Article.new(jk_rowling, maxim, "How to Diet", "Diet info")
+champion = Article.new(nick_sparks, sports_illustrated, "Virginia National Champs", "NCAA Chamionship")
 
 
 
