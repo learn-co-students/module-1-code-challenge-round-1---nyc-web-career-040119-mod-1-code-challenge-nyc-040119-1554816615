133f6b6fa4d7d36598f0f2077d0b5421b8eb5a8c
Jacob Lam <jlam@binghamton.edu>
2019-04-09 10:41:45 -0400

    Did My Best

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,44 @@
 class Author
   attr_accessor :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
-  
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end
 
+  def articles
+    Article.all.select{|article| article.author== self }
+  end
+
+  def add_article(magazine,title,content)
+    Article.new(self,magazine,title,content)
+  end
+
+  def magazines
+    articles.map{|article| article.magazine}.uniq
+  end
+
+  def show_specialties
+    # binding.pry
+    self.magazines.map{|magazine| magazine.category}
+  end
+
+  def self.most_active #incomplete
+    hash = {}
+    @@all.each do |author|
+    hash[author] = author.articles.length
+      end
+      binding.pry
+      hash.values.max
+    end
+
+  def self.most_verbose
+    Article.all.max_by {|article| article.content.length }.author
+  end
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,11 +1,38 @@
 class Magazine
   attr_accessor :name, :category
 
+  @@all = []
 
   def initialize(name, category)
     @name = name
     @category = category
+    @@all << self
 
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    # binding.pry
+    @@all.select {|magazine| magazine.name == name}
+  end
+
+  def articles #helper method all the articles tied to this magazine
+    Article.all.select{|article| article.magazine == self}
+  end
+
+  def article_titles
+    articles.map {|article| article.title}
+  end
+
+  def contributors
+    self.articles.map {|article| article.author}.uniq
+  end
+
+  def word_count
+    articles.map{|article| article.content}.join(" ").split(" ").length
   end
 
 
