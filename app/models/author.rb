8dcb52064a467adf549c6666f2ff4b41a6692cb5
Gamerman2001 <Tro12185@gmail.com>
2019-04-09 10:48:34 -0400

    lab

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,11 +1,52 @@
 class Author
-  attr_accessor :name
+  attr_reader :name
 
-
+  @@all = []
   def initialize(name)
     @name = name
-  
+    @@all << self 
+  end
+  #instance variables 
+  def add_article(author,magazine,title,content)
+      new_article = Article.new(self, magazine, title, content)
   end
 
+  def articles 
+    Article.all.select do |article|
+      # binding.pry
+      article.author.name == self.name
+    end.uniq
+  end
+
+  def magazines 
+    articles.select do |article|
+      article.author.name == self.name 
+      # binding.pry
+    end
+  end
+
+  def show_specialties
+    category = []
+    magazines.select do |magazine|
+      category << magazine.magazine.category
+      # binding.pry
+    end
+    category.uniq
+  end
+
+  
+  #class variables
+  def self.all
+    @@all 
+  end
+
+  def self.most_active 
+    author = []
+    Article.all.select do |articles|
+      author << articles.author
+    end 
+    # binding.pry
+    author
+  end
 
 end
--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,12 +1,38 @@
 class Magazine
   attr_accessor :name, :category
 
-
+  @@all = []
   def initialize(name, category)
     @name = name
     @category = category
-
+    @@all << self 
   end
+
+  #class methods
+  def self.all 
+    @@all
+  end 
+
+  def self.find_by_name(name)
+    self.all.find do |magazine|
+      magazine.name == name 
+    end
+  end 
+
+  def article_titles
+    Article.all.map do |magazine|
+      magazine.title
+      # binding.pry
+    end 
+  end
+
+  def contributors 
+    Article.all.select do |magazine|
+      magazine.author
+      # binding.pry
+    end
+  end
+
 
 
 end
